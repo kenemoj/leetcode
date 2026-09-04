23e5c775b41063d9c3f74503f4f322fafc1a4bab
Kene Mojekwu <106906125+kenemoj@users.noreply.github.com>
2026-09-03 19:51:38 -0500

    Time: 3 ms (17.12%) | Memory: 44 MB (30.71%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] res = new int[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            if(k > 0)
+            {
+                for(int j = i+1; j < i+1+k; j++)
+                {
+                    res[i] += code[j % n];
+                }
+            }else if(k < 0)
+            {
+                for(int j = i-1; j > i-1-Math.abs(k); j--)
+                {
+                    res[i] += code[((j % n) + n) % n];
+                }
+            }
+        }
+        return res; 
+    }
+}
